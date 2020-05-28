5e593c1f981aa93665da0c66b7c457da62db2f8c
Amanda Ng <amandaleahng@gmail.com>
2020-05-28 16:52:31 +0800

    fixing schema for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_035919) do
+ActiveRecord::Schema.define(version: 2020_05_28_034315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
