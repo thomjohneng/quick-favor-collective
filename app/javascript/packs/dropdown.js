b960d97584773ea09b71c3b4702a56f5ce895038
Amanda Ng <amandaleahng@gmail.com>
2020-05-30 14:08:14 +0800

    dropdown working

--- app/javascript/packs/dropdown.js
+++ app/javascript/packs/dropdown.js
@@ -7,3 +7,27 @@
 //     )
 // })
 
+const cityInput = document.getElementById('cities');
+const industryInput = document.getElementById('industries')
+// const table = document.querySelector('#userTable');
+const tableRow = document.querySelectorAll('.userRow');
+
+cityInput.addEventListener("change", (e) => {
+  const city = cityInput.value
+  tableRow.forEach((row) => {
+    row.classList.remove("inactive-row")
+    if (row.innerHTML.includes(city) === false) {
+      row.classList.add("inactive-row")
+    }
+  })
+})
+
+industryInput.addEventListener("change", (e) => {
+  const industry = industryInput.value
+  tableRow.forEach((row) => {
+    row.classList.remove("inactive-row")
+    if (row.innerHTML.includes(industry) === false) {
+      row.classList.add("inactive-row")
+    }
+  })
+})
