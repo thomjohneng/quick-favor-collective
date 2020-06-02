25dd4a2d3e1139d538b568b907417ff7b57ed2e3
Amanda Ng <amandaleahng@gmail.com>
2020-06-02 09:37:06 +0800

    changed action mailer based settings to authentication plain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "quick-favor-club.herokuapp.com" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
